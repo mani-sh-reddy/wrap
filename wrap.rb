a1ae6f18b69cf9831352e695618638061795c123
Mani Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2025-03-09 20:42:27 +0000

    Updated brew ruby file

--- wrap.rb
+++ wrap.rb
@@ -1,15 +1,15 @@
 class Wrap < Formula
-  desc "A collection of bash utilities wrapped and accessible via keywords"
+  desc "Bash utilities collection wrapped and accessible via keywords"
   homepage "https://github.com/mani-sh-reddy/wrap"
-  url "https://github.com/mani-sh-reddy/wrap", tag: "0.0.1"
+  url "https://github.com/mani-sh-reddy/wrap", tag: "v0.0.1"
   license "MIT"
-  head "https://github.com/mani-sh-reddy/wrap.git"
+  head "https://github.com/mani-sh-reddy/wrap.git", branch: "main"
 
   def install
     bin.install "wrap"
   end
 
   test do
-    system "#{bin}/wrap", "select", "--options", "a", "b"
+    system bin/"wrap", "select", "--options", "a", "b"
   end
 end
